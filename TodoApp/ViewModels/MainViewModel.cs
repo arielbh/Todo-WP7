6d69f1d2295984f4f732fa31ff56ad716e38586c
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-06-12 00:22:29 +0300

    Initial Commit for WP7 with VM

---
+++ TodoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TodoApp.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _title = "Today";
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+
+    }
+}
---
+++ TodoApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace TodoApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
